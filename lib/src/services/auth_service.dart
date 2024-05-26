4a0751088dd186e8abd258ef8ec5883ba2c7027f
ElcioLS <elciols@gmail.com>
2024-05-26 14:56:34 -0300

    criando repository e service

---
+++ lib/src/services/auth_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthService {
+  final FlutterSecureStorage _storage;
+
+  AuthService({required FlutterSecureStorage storage}) : _storage = storage;
+
+  // Future<bool> login()async{
+  //logar
+  //gravar token
+  //recuperar token
+  // }
+}
